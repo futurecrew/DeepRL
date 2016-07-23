0987bc2e2b64a695bf89dd74429b066f6de92890
dj <futurecrew@gmail.com>
2016-07-23 16:29:18 +0900

    A commit

--- deep_rl_player.py
+++ deep_rl_player.py
@@ -51,8 +51,46 @@ key_action_tform_table = (
 )
 
 class DeepRLPlayer:
-    def __init__(self):
+    def __init__(self, showScreen):
+        self.showScreen = showScreen
         self.grayPixels = np.zeros((84, 84), np.float)
+        
+        self.rgb_palette = [
+          0x000000, 0, 0x4a4a4a, 0, 0x6f6f6f, 0, 0x8e8e8e, 0,
+          0xaaaaaa, 0, 0xc0c0c0, 0, 0xd6d6d6, 0, 0xececec, 0,
+          0x484800, 0, 0x69690f, 0, 0x86861d, 0, 0xa2a22a, 0,
+          0xbbbb35, 0, 0xd2d240, 0, 0xe8e84a, 0, 0xfcfc54, 0,
+          0x7c2c00, 0, 0x904811, 0, 0xa26221, 0, 0xb47a30, 0,
+          0xc3903d, 0, 0xd2a44a, 0, 0xdfb755, 0, 0xecc860, 0,
+          0x901c00, 0, 0xa33915, 0, 0xb55328, 0, 0xc66c3a, 0,
+          0xd5824a, 0, 0xe39759, 0, 0xf0aa67, 0, 0xfcbc74, 0,
+          0x940000, 0, 0xa71a1a, 0, 0xb83232, 0, 0xc84848, 0,
+          0xd65c5c, 0, 0xe46f6f, 0, 0xf08080, 0, 0xfc9090, 0,
+          0x840064, 0, 0x97197a, 0, 0xa8308f, 0, 0xb846a2, 0,
+          0xc659b3, 0, 0xd46cc3, 0, 0xe07cd2, 0, 0xec8ce0, 0,
+          0x500084, 0, 0x68199a, 0, 0x7d30ad, 0, 0x9246c0, 0,
+          0xa459d0, 0, 0xb56ce0, 0, 0xc57cee, 0, 0xd48cfc, 0,
+          0x140090, 0, 0x331aa3, 0, 0x4e32b5, 0, 0x6848c6, 0,
+          0x7f5cd5, 0, 0x956fe3, 0, 0xa980f0, 0, 0xbc90fc, 0,
+          0x000094, 0, 0x181aa7, 0, 0x2d32b8, 0, 0x4248c8, 0,
+          0x545cd6, 0, 0x656fe4, 0, 0x7580f0, 0, 0x8490fc, 0,
+          0x001c88, 0, 0x183b9d, 0, 0x2d57b0, 0, 0x4272c2, 0,
+          0x548ad2, 0, 0x65a0e1, 0, 0x75b5ef, 0, 0x84c8fc, 0,
+          0x003064, 0, 0x185080, 0, 0x2d6d98, 0, 0x4288b0, 0,
+          0x54a0c5, 0, 0x65b7d9, 0, 0x75cceb, 0, 0x84e0fc, 0,
+          0x004030, 0, 0x18624e, 0, 0x2d8169, 0, 0x429e82, 0,
+          0x54b899, 0, 0x65d1ae, 0, 0x75e7c2, 0, 0x84fcd4, 0,
+          0x004400, 0, 0x1a661a, 0, 0x328432, 0, 0x48a048, 0,
+          0x5cba5c, 0, 0x6fd26f, 0, 0x80e880, 0, 0x90fc90, 0,
+          0x143c00, 0, 0x355f18, 0, 0x527e2d, 0, 0x6e9c42, 0,
+          0x87b754, 0, 0x9ed065, 0, 0xb4e775, 0, 0xc8fc84, 0,
+          0x303800, 0, 0x505916, 0, 0x6d762b, 0, 0x88923e, 0,
+          0xa0ab4f, 0, 0xb7c25f, 0, 0xccd86e, 0, 0xe0ec7c, 0,
+          0x482c00, 0, 0x694d14, 0, 0x866a26, 0, 0xa28638, 0,
+          0xbb9f47, 0, 0xd2b656, 0, 0xe8cc63, 0, 0xfce070, 0
+        ]
+        
+
     
     def getActionFromKeys(self):
         #get the keys
@@ -102,19 +140,59 @@ class DeepRLPlayer:
             plt.imshow(rgb)
         plt.show()
 
-    def getScreenPixels(self, ale, screen, game_surface):
+    def getScreenPixels(self, ale, screen, game_surface, step):
+        
         numpy_surface = np.frombuffer(game_surface.get_buffer(),dtype=np.int32)
         ale.getScreenRGB(numpy_surface)
+        
+        '''
+        s1 = np.zeros((33600,), dtype=np.int32)
+        ale.getScreenRGB(s1)
+        data = s1.view(np.uint8).reshape(s1.shape + (4,))
+        rgba = np.reshape(data, (self.screen_height, self.screen_width, 4))
+        rgb = rgba[:, :, (2, 1, 0)]        
+        cropped = rgb.astype(np.float) / 256
+        #self.display(cropped)
+        
+        
+        s2 = np.zeros((210*160,), dtype=np.uint8)
+        ale.getScreen(s2)
+        rgba2 = np.reshape(s2, (self.screen_height, self.screen_width))
+        resized = imresize(rgba2, (110, 84, 3))
+        cropped = resized[26:110, :].astype(np.float) / 256        
+        
+        newRgba2 = np.zeros((210, 160, 3), dtype=np.uint32)
+        for y in range(210):
+            for x in range(160):
+                #data = rgba2[y, x]
+                data = rgba2[y, x] & ~0x1
+                a = self.rgb_palette[data]
+                
+                if a != 0:
+                    r = (a & 0xFF0000) >> 16;
+                    g = (a & 0x00FF00) >> 8;
+                    b = a & 0x0000FF; 
+                    newRgba2[y, x, 0] = r
+                    newRgba2[y, x, 1] = g
+                    newRgba2[y, x, 2] = b
+        '''
+        
         #del numpy_surface
+        
 
-        screen.blit(pygame.transform.scale2x(game_surface),(0,0))
+        if (self.showScreen):
+            screen.fill((0,0,0))
+            screen.blit(pygame.transform.scale2x(game_surface),(0,0))
+            #screen.blit(game_surface.copy(),(0,0))
 
         data = numpy_surface.view(np.uint8).reshape(numpy_surface.shape + (4,))
         rgba = np.reshape(data, (self.screen_height, self.screen_width, 4))
         rgb = rgba[:, :, (2, 1, 0)]
         resized = imresize(rgb, (110, 84, 3))
-        cropped = resized[26:110, :, :].astype(np.float) / 256        
-        #self.display(cropped)
+        cropped = resized[26:110, :, :].astype(np.float) / 256
+        
+        #if step > 200:        
+        #    self.display(cropped)
         
         self.grayPixels = np.dot(cropped[...,:3], [0.299, 0.587, 0.114])        
         #self.display(self.grayPixels, gray=True)
@@ -150,45 +228,46 @@ class DeepRLPlayer:
         
         (display_width,display_height) = (1024,420)
         
-        #init pygame
         pygame.init()
-        screen = pygame.display.set_mode((display_width,display_height))
-        pygame.display.set_caption("Arcade Learning Environment Player Agent Display")
-        
         game_surface = pygame.Surface((self.screen_width,self.screen_height))
+
+        if self.showScreen:
+            screen = pygame.display.set_mode((display_width,display_height))
+            pygame.display.set_caption("Arcade Learning Environment Player Agent Display")
+            
+            pygame.display.flip()
+        else:
+            screen = None
         
-        pygame.display.flip()
-        
-        #init clock
         clock = pygame.time.Clock()
         
         episode = 0
         total_reward = 0.0 
+        step = 0
         while(episode < 10):
-            a, pressed = self.getActionFromKeys()
+            if self.showScreen:
+                a, pressed = self.getActionFromKeys()
+            else:
+                a = 1
 
             reward = ale.act(a);
             total_reward += reward
         
-            #clear screen
-            screen.fill((0,0,0))
+            pixels = self.getScreenPixels(ale, screen, game_surface, step)
         
-            #get atari screen pixels and blit them
-            pixels = self.getScreenPixels(ale, screen, game_surface)
-        
-            #get RAM
             ram_size = ale.getRAMSize()
             ram = np.zeros((ram_size),dtype=np.uint8)
             ale.getRAM(ram)
             
-            #Display info
-            self.displayInfo(screen, ram, a, total_reward)
+            if self.showScreen:
+                self.displayInfo(screen, ram, a, total_reward)
+                pygame.display.flip()
         
-            pygame.display.flip()
+                if(self.checkExit(pressed)):
+                    break
         
-            if(self.checkExit(pressed)):
-                break
-        
+            step +=1 
+            
             #delay to 60fps
             #clock.tick(60.)
         
@@ -207,4 +286,7 @@ if __name__ == '__main__':
         print("Usage ./ale_python_test_pygame_player.py <ROM_FILE_NAME>")
         sys.exit()
     
-    DeepRLPlayer().gogo()
+    showScreen = True
+    #showScreen = False
+    
+    DeepRLPlayer(showScreen).gogo()
