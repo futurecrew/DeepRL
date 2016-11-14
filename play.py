a8b6d4e64c826614cfe3367aa984258564f37556
dj <futurecrew@gmail.com>
2016-11-15 01:02:45 +0900

    Change command option of play.py

--- play.py
+++ play.py
@@ -3,9 +3,8 @@ from deep_rl_train import DeepRLPlayer
         
 if __name__ == '__main__':
     args = get_args()
-    args.replay_file = 'snapshot/%s/%s' % (args.game, '20161030_144636/dqn_2404871')
     if args.replay_file == None:
-        print 'Usage: python player.py replay_file'
+        print 'Usage: python player.py [path_to_rom_file] --replay-file [path_to_snapshot_file]'
         exit()
 
     print 'Play using data_file: %s' % args.replay_file
